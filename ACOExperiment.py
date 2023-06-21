af69d0a2f2069ded2cd88d18b4852153cd4d364c
agudroid <agudonio@gmail.com>
2023-06-21 13:33:13 +0200

    fixes

--- ACOExperiment.py
+++ ACOExperiment.py
@@ -8,12 +8,10 @@ num_ants = 2
 num_iterations = 10
 alpha = 1.0
 beta = 1.0
-evaporation_rate = 0.5
-graphList = []
-modularity1 = []
-modularity2 = []
-modularity3 = []
-for i in range(1):
+evaporation_rate = 0.25
+experiments = []
+for i in range(10):
+    dataDicc = {}
     graph = nx.barbell_graph(3,1)
     instance_name = 'graph_barbell'+str(i)+'.gml'
     for node, data in graph.nodes(data=True):
@@ -22,32 +20,22 @@ for i in range(1):
                 data[key] = ", ".join(map(str, value))
     path = instance_name
     nx.write_gml(graph,path)
-    graphList.append(instance_name)
+    dataDicc['Instancia'] = path
     graph = dgl.from_networkx(graph)
     communities,modularity = aco(graph, num_ants, num_iterations, alpha, beta, evaporation_rate)
-    modularity1.append(modularity)
+    dataDicc['Modularidad1'] = modularity
     evaporation_rate = 0.5
     communities,modularity = aco(graph, num_ants, num_iterations, alpha, beta, evaporation_rate)
-    modularity2.append(modularity)
+    dataDicc['Modularidad2'] = modularity
     evaporation_rate = 0.75
     communities,modularity = aco(graph, num_ants, num_iterations, alpha, beta, evaporation_rate)
-    modularity3.append(modularity)
+    dataDicc['Modularidad3'] = modularity
+    experiments.append(dataDicc)
 
-dict1 = {'Instancia': graphList}
-print(dict1)
-dict2 = {'modularidad': modularity1}
-print(dict2)
-dict3 = {'modularidad': modularity2}
-print(dict3)
-dict4 = {'modularidad': modularity3}
-print(dict4)
-df1 = pd.DataFrame.from_dict(dict1, orient='index', columns=['Column1'])
-df2 = pd.DataFrame.from_dict(dict2, orient='index', columns=['Column2'])
-df3 = pd.DataFrame.from_dict(dict3, orient='index', columns=['Column3'])
-df4 = pd.DataFrame.from_dict(dict4, orient='index', columns=['Column4'])
-
-# Concatenar los DataFrames verticalmente
-df_concat = pd.concat([df1, df2, df3,df4], axis=0)
+df = pd.DataFrame(experiments)
 
 # Guardar el DataFrame en un archivo CSV
-df_concat.to_csv('datos.csv', index=True)
+columns = list(experiments[0].keys())
+
+print(experiments[0].keys())
+df.to_csv('datos.csv', index=True, header=columns)
